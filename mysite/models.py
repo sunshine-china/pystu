adf77458c50e6e00108e12a5d5d321a004eec5b1
wangjianjian <wjj_ly@126.com>
2016-03-10 15:53:11 +0800

    视图和模板基础

--- mysite/models.py
+++ mysite/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.utils import  timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
      question_text=models.CharField(max_length=200)
      pub_date=models.DateField("date published")
+     was_published_recently=models.BooleanField(default=False)
 
      def __str__(self):
          return self.question_text
+
+     def was_published_recently(self):
+         return True
+     was_published_recently.admin_order_field='pub_date'
+     was_published_recently.boolean=True
+     was_published_recently.short_description='Publish recently?'
 
 class Choice(models.Model):
     question=models.ForeignKey(Question)
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import  HttpResponse
+from django.template import  RequestContext,loader
+from .models import Question
 # Create your views here.
+
+def index(request):
+    last_question_list=Question.objects.order_by("-pub_date")[:5]
+    template=loader.get_template("mysite/index.html")
+    context=RequestContext(request,{'last_question_list':last_question_list})
+    return HttpResponse(template.render(context))
+
+
+def detail(request,question_id):
+    return HttpResponse("you're looking at question:%s" % question_id)
+
+def results(request,question_id):
+    response="your'are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("Your'are voting on question %s." % question_id)
